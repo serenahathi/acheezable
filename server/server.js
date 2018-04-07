ad9c90962e69ebf37881e00d5cdef39246d42a4a
Lewis Watson <lewwwiswatson@gmail.com>
2018-04-07 18:20:51 +0100

    :fire: Move update functionality into server.js and delete app.js

--- server/server.js
+++ server/server.js
@@ -24,12 +24,25 @@ app.post('/', (req, res) => {
 
 app.get('/acheezements', (req, res) => {
   // get data (eventually find by date)
-  Goal.find({ "createdAt": { 
+  Goal.find({ "createdAt": {
     $lt: new Date(),
     $gte: new Date(new Date().setDate(new Date().getDate()-1))}
    }, function(err, allGoals) {
     err ? console.log(err) : res.render("acheezements", { goals: allGoals });
   })
+});
+
+app.post('/update', (req, res) => {
+  console.log(req.body.id)
+  Goal.findById({_id: req.body.id}, function (err, goal) {
+    if (err) return console.log(err);
+
+    goal.completed = true;
+    goal.save(function (err, updatedGoal) {
+      if (err) return console.log(err);
+      res.redirect('/acheezements');
+    });
+  });
 });
 
 app.listen(3000, () => {
