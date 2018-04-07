4caa8d2f3e0dbf1646d2f97b520b6170b413d17e
Serena Hathi <hathi.serena@gmail.com>
2018-04-07 17:51:44 +0100

    :fire: Remove duplicated test folder

--- tests/sample.js
+++
@@ -1,9 +0,0 @@
-function cheezeMoulder(arr) {
-  for (var i = 0; i < arr.length; i++) {
-    arr[i] = 'Blue ' + arr[i]
-  }
-  return arr;
-}
-
-
-module.exports = cheezeMoulder
--- tests/sample.test.js
+++
@@ -1,8 +0,0 @@
-const cheezeMoulder = require('./sample');
-
-describe('makeCheezesBlue', () => {
-  it('makes each cheeze blue', () => {
-    const actual = cheezeMoulder(['Stilton', 'Halloumi', 'Cheddar']);
-    expect(actual).toEqual(['Blue Stilton', 'Blue Halloumi', 'Blue Cheddar']);
-  });
-});
