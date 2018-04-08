c9530984a0cd07e7ae109d2b5083b85285eb6796
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2018-04-08 20:36:07 +0100

    :construction: add route for login post method

--- server/server.js
+++ server/server.js
@@ -68,7 +68,11 @@ app.get('/login', (req, res) => {
   res.render('login.ejs', { message: req.flash('loginMessage') });
 });
 
-// app.post('/login', do passport stuff here);
+app.post('/login', passport.authenticate('local-login', {
+  successRedirect : '/profile',
+  failureRedirect : '/login',
+  failureFlash : true
+}));
 
 app.get('/signup', (req, res) => {
   res.render('signup.ejs', { message: req.flash('signupMessage') });
