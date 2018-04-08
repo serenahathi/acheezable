aa5930972e1b58a84d166f3f98b3d5495c958968
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2018-04-08 20:21:36 +0100

    :construction: setup passport.js

---
+++ server/config/passport.js
@@ -0,0 +1,40 @@
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user');
+
+module.exports = function(passport) {
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+  passport.use('local-signup', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  }, 
+  funtion(req, email, password, done) {
+    process.nextTick(function() {
+      User.findOne({ 'local.email' : email }, function(err, user) {
+        if (err)
+          return done(err);
+        if (user) {
+          return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+        } else {
+          let newUser = new User();
+          newUser.local.email = email;
+          newUser.local.password = newUser.generateHash(password);
+          newUser.save(function(err) {
+            if (err)
+              throw err;
+            return done(null, newUser);
+          });
+        }
+      });
+    });
+  }));
+};
