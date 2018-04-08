c59d52bc7c1d508148040332658d7ff8e3c52e74
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2018-04-08 18:35:16 +0100

    :construction: create routes for user auth

--- server/server.js
+++ server/server.js
@@ -64,6 +64,35 @@ app.post('/update', (req, res) => {
   });
 });
 
+app.get('/login', (req, res) => {
+  res.render('login.ejs', { message: req.flash('loginMessage') });
+});
+
+// app.post('/login', do passport stuff here);
+
+app.get('/signup', (req, res) => {
+  res.render('signup.ejs', { message: req.flash('signupMessage') });
+});
+
+// app.post('/signup.ejs', do passport stuff here);
+
+app.get('/profile', isLoggedIn, (req, res) => {
+  res.render('profile.ejs', {
+    user : req.user
+  });
+});
+
+app.get('/logout', (req, res) {
+  req.logout();
+  res.redirect('/login');
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated())
+    return next();
+  res.redirect('/login');
+;}
+
 app.listen(3000, () => {
   console.log("Started on port 3000")
 });
