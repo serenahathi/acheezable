11ba6e9a705a08b22091d98009c963c7836ca3f6
Serena Hathi <hathi.serena@gmail.com>
2018-04-09 17:23:11 +0100

    :sparkles: Add ability to edit goal

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const flash = require('connect-flash');
+const _ = require('lodash');
 // const morgan = require('morgan'); // don't think we need this
 
 require('./config/passport')(passport);
@@ -44,20 +45,27 @@ app.get('/acheezements', isLoggedIn, (req, res) => {
     $gte: new Date(new Date().setDate(new Date().getDate()-1))}
    }, function(err, allGoals) {
     err ? console.log(err) : res.render("goals/index", { goals: allGoals });
-  })
+  });
 });
 
-app.get('/acheezements/:id/edit', (req, res) => {
+app.get('/acheezements/:id/edit', isLoggedIn, (req, res) => {
   Goal.findById({_id: req.params.id}, function (err, goal) {
     if (err) return console.log(err);
 
     res.render('goals/edit', {goal: goal})
-  })
-})
+  });
+});
 
-app.patch('/acheezements/:id', (req, res) => {
-  Goal.findById({_id: })
-})
+app.post('/acheezements/:id', isLoggedIn, (req, res) => {
+  Goal.findById({_id: req.params.id}, function (err, goal) {
+    if (err) return console.log(err);
+    goal.text = req.body.goal;
+    goal.save(function (err, goal) {
+      if (err) return console.log(err);
+      res.redirect('/acheezements');
+    })
+  });
+});
 
 app.post('/acheezements', isLoggedIn, (req, res) => {
   Goal.create({
