af5c1bc8972ab467fa51a9cfccd2f3c2c4a99479
Lewis Watson <lewwwiswatson@gmail.com>
2018-04-09 13:20:49 +0100

    :construction: Use loops in view to show completed goals

--- public/src/cheezetoryInterface.js
+++ public/src/cheezetoryInterface.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-console.log(goalHistory)
+  console.log($('#day0'))  //.html().trim().split(','))
+  let completionArray = $('.goals').html().trim().split(',');
+
+  // $('.goals').forEach((goal) => {
+  //   console.log(goal)
+  // });
+
+  // console.log(Array.from($('.goals'))[0].html())
+
+console.log($('.goals').length)
+
+  for (var i = 0; i < $('.goals').length; i++) {
+    console.log($(`#day[i]`))
+  }
 });
