9ae01d575f733600455063dd4cc5a7d808fe2295
Lewis Watson <lewwwiswatson@gmail.com>
2018-04-10 20:21:02 +0100

    :bug: Fix goals showing as zero when no goals set in cheeztory

--- public/src/cheezetoryInterface.js
+++ public/src/cheezetoryInterface.js
@@ -1,20 +1,21 @@
 $(document).ready(function() {
 
- $('.days').click(function() {
+  $('.days').click(function() {
    let i = $(this).val()
-   let completionArray = $(`#day${i}`).html().trim().split(',');
-   let newHTML = ''
-   for (var j = 0; j < (completionArray.length); j += 2) {
-     if (completionArray[j + 1] === "true") {
-       newHTML += `<div class="true">${completionArray[j]}  </div>`;
-     } else {
-       newHTML += `<div class="false">${completionArray[j]}  </div>`;
+   if ($(`#day${i}`).html()) {
+     let completionArray = $(`#day${i}`).html().trim().split(',');
+     let newHTML = ''
+     for (var j = 0; j < (completionArray.length); j += 2) {
+       if (completionArray[j + 1] === "true") {
+         newHTML += `<div class="true">${completionArray[j]}  </div>`;
+       } else {
+         newHTML += `<div class="false">${completionArray[j]}  </div>`;
+       }
      }
+     $(`#day${i}`).html(newHTML);
+     $('.true').css('color', 'green');
+     $('.false').css('color', 'red');
+     $(`#day${i}`).toggleClass('hide');
    }
-   $(`#day${i}`).html(newHTML);
-   $('.true').css('color', 'green');
-   $('.false').css('color', 'red');
-   $(`#day${i}`).toggleClass('hide');
- });
-
+  });
 });
