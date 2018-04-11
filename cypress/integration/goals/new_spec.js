2a75a62e78828d6c697d32d347f7b728fe56f0fc
Serena Hathi <hathi.serena@gmail.com>
2018-04-11 14:42:18 +0100

    :white_check_mark: Test ability to add and view goals

--- cypress/integration/goals/new_spec.js
+++ cypress/integration/goals/new_spec.js
@@ -1,58 +1,52 @@
-describe('Acheezements page', function() {
+describe('Add new acheezement', function() {
 
-    it('<logs user in>', function() {
-      cy.visit('http://localhost:3000/acheezements')
-      cy.get('#emailAddress').type('cypress@test.com')
-      cy.get('#password').type('password')
-      cy.get('#login').click()
-    })
-  
-it('checks title is `Acheezable`', function() {
+  beforeEach(function() {
+    cy.visit('http://localhost:3000')
+    cy.get('#emailAddress').type('cypress@test.com')
+    cy.get('#password').type('password')
+    cy.get('#login').click()
+  })
+
+  it('checks title is `Acheezable`', function() {
     cy.get('title').contains('Acheezable')
   })
 
   it('checks header is `Create Acheezements`', function() {
     cy.get('h1').contains('Create Acheezements')
   })
-});
 
-describe('Adding a new goal', function() {
-  // it('allows user to input and submit one goal', function() {
-  //   cy.get('#inputGoal').type('Meditate')
-  //   cy.get('#addGoal').click();
-  // })
-
-  it('navigates to the View Acheezements page and shows the newly entered goal', function() {
+  it('allows user to input a goal and view it on the "View Acheezements" page', function() {
+    cy.get('#inputGoal').type('Meditate')
+    cy.get('#addGoal').click();
     cy.get('#viewGoals').click();
-    // cy.get('body').contains("Meditate")
-  });
+    cy.get('body').contains("Meditate")
+  })
 
-  it('allows user to input and submit a second goal', function() {
-    cy.get('#back-button').click();    
+  it('allows user to input and submit a second goal and view both goals on the "View Acheezements" page', function() {
     cy.get('#inputGoal').type('Go for a run')
     cy.get('#addGoal').click();
     cy.get('#viewGoals').click();
-  })
-
-  it('navigates to the View Acheezements page and shows both goals', function() {
-    // Note: adding line 31 here prompts the user to log in again. Not sure why.
     cy.get('body').contains("Meditate")
     cy.get('body').contains("Go for a run")
-  });
+  })
 
-  // it('displays a suggested goal when the user clicks "Need a suggestion"', function(){
-  //   cy.get('#goalSuggestion').click();
-  //   cy.get('#suggestionDisplay').contains('')
-  // })
+  it('displays a suggested goal when the user clicks "Need a suggestion"', function(){
+    cy.get('#goalSuggestion').click();
+    expect('#suggestionDisplay').to.be.a('string')
+  })
 
-  // it('adds suggested goal to View Acheezements page when the user clicks "Accept sugestion"', function(){
-  //   cy.get('#goalSuggestion').click();
-  //   cy.get('#suggestionDisplay').contains('')
-  // })
+  it( 'navigates to the sign in page when the user clicks on "Log out"', function() {
+    cy.get('#logout').click();
+    cy.url().should('eq', 'http://localhost:3000/login')    
+  })
 
-   // it( 'navigates to the sign in page when the user clicks on "Log out"', function() {
-  //   cy.get('#logout').click();
-  //   cy.url().should('eq', 'http://localhost:3000/login')    
-  // })
-
-})
+//   it('adds suggested goal to View Acheezements page when the user clicks "Accept sugestion"', function(){
+//     cy.get('#goalSuggestion').click();
+//     cy.get('#suggested-goal').then(() => {
+//       // const suggestedGoal = ('#suggested-goal').text
+//     cy.get('#acceptSuggestion').click();
+//     cy.get('#viewGoals').click();
+//     cy.get('body').contains('#suggested-goal').text
+//   })
+// })
+});
