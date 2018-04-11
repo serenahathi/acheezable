bd120bbbd5fa584d12d9ffd80c71d82c9c9edecc
Serena Hathi <hathi.serena@gmail.com>
2018-04-11 11:32:25 +0100

    Add back button

--- cypress/integration/goals/new_spec.js
+++ cypress/integration/goals/new_spec.js
@@ -25,13 +25,30 @@ describe('Adding a new goal', function() {
     cy.get('button').click()
   })
 
-  it('allows user to input and submit a goal', function() {
+  it('allows user to input and submit one goal', function() {
     cy.get('#inputGoal').type('Meditate')
     cy.get('#addGoal').click();
     cy.get('#viewGoals').click();
   })
 
-  it('navigates to the View Acheezements page and see the newly entered goal', function() {
+  it('navigates to the View Acheezements page and shows the newly entered goal', function() {
+    // Note: adding line 31 here prompts the user to log in again. Not sure why.
     cy.get('body').contains("Meditate")
   });
+
+  it('allows user to input and submit a second goal', function() {
+    cy.get('#inputGoal').type('Go for a run')
+    cy.get('#addGoal').click();
+    cy.get('#viewGoals').click();
+  })
+
+  it('navigates to the View Acheezements page and shows both goals', function() {
+    // Note: adding line 31 here prompts the user to log in again. Not sure why.
+    cy.get('body').contains("Meditate")
+    cy.get('body').contains("Go for a run")
+  });
+
+
+
+
 });
