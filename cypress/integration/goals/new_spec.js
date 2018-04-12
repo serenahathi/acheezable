64648f812f6777397626a23fc399da0538ee6e67
Serena Hathi <hathi.serena@gmail.com>
2018-04-12 12:47:31 +0100

    ::white_check_mark:: Tests for suggestion feature

--- cypress/integration/goals/new_spec.js
+++ cypress/integration/goals/new_spec.js
@@ -15,38 +15,30 @@ describe('Add new acheezement', function() {
     cy.get('h1').contains('Create Acheezements')
   })
 
+  it('displays a suggested goal when the user clicks "Need a suggestion"', function(){
+    cy.get('#goalSuggestion').click()
+    expect('#suggestedGoal').to.be.a('string')
+  })
+
+  it('adds the suggestion when the accept button is clicked', function() {
+    cy.get('#goalSuggestion').click()
+    cy.get('#acceptSuggestion').click()
+    cy.get('#viewGoals').click()
+    expect('li').to.be.a('string')
+  })
+
   it('allows user to input a goal and view it on the "View Acheezements" page', function() {
     cy.get('#inputGoal').type('Meditate')
-    cy.get('#addGoal').click();
-    cy.get('#viewGoals').click();
+    cy.get('#addGoal').click()
+    cy.get('#viewGoals').click()
     cy.get('body').contains("Meditate")
   })
 
   it('allows user to input and submit a second goal and view both goals on the "View Acheezements" page', function() {
     cy.get('#inputGoal').type('Go for a run')
-    cy.get('#addGoal').click();
-    cy.get('#viewGoals').click();
+    cy.get('#addGoal').click()
+    cy.get('#viewGoals').click()
     cy.get('body').contains("Meditate")
     cy.get('body').contains("Go for a run")
   })
-
-  it('displays a suggested goal when the user clicks "Need a suggestion"', function(){
-    cy.get('#goalSuggestion').click();
-    expect('#suggestionDisplay').to.be.a('string')
-  })
-
-  it( 'navigates to the sign in page when the user clicks on "Log out"', function() {
-    cy.get('#logout').click();
-    cy.url().should('eq', 'http://localhost:3000/login')    
-  })
-
-//   it('adds suggested goal to View Acheezements page when the user clicks "Accept sugestion"', function(){
-//     cy.get('#goalSuggestion').click();
-//     cy.get('#suggested-goal').then(() => {
-//       // const suggestedGoal = ('#suggested-goal').text
-//     cy.get('#acceptSuggestion').click();
-//     cy.get('#viewGoals').click();
-//     cy.get('body').contains('#suggested-goal')
-//   })
-// })
 });
