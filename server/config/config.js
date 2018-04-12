c988b3a15946134af04bc09a35794d19ffe06033
Terry Allitt <tallitt88@gmail.com>
2018-04-12 11:15:18 +0100

    ::wrench: improve config setup for test and development environments - update README to instruct seeding of data

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,11 @@
 const env = process.env.NODE_ENV || 'development';
 console.log('env ****', env);
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://heroku_2bkcpqtl:te5p15oamr008ndu15578n12g6@ds241019.mlab.com:41019/heroku_2bkcpqtl';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/AcheezableTest';
-}
+if (env === 'development' || env === 'test') {
+  const config = require('./config.json');
+  let envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
+};
